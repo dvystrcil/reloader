6a07c2069953f8f183f4983716dc2af12e52e703
Dan Vystrcil <dvystrcil@gmail.com>
2024-02-11 09:01:08 -0800

    first commit

---
+++ tools/add-version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# if you did not pass in version then fail
+if [ "${1}" == "" ];
+then
+    echo "You need to provide a version of reload, with a 'v'"
+    echo "example:"
+    echo "   v0.13.10"
+    exit 1
+fi
+
+version="${1}"
+folder="kustomize/base/release/${version}"
+
+if [ ! -d ${folder} ];
+then    
+    mkdir -p "${folder}"
+    cd "${folder}"
+    curl -s -O "https://raw.githubusercontent.com/stakater/Reloader/${version}/deployments/kubernetes/reloader.yaml"
+    cp ../../../../templates/base-release-template.yaml kustomization.yaml
+    cd ../../../../
+fi
